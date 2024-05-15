ed00f0b368b970d1c276807be5e3a94f8ca7c651
Nagisa-Ogawa <o.nagisa0123@gmail.com>
2024-05-15 10:34:15 +0900

    ・プレイヤーの攻撃処理を追加しました ・プレイヤーのコントローラーでの移動処理を追加しました。 ・カメラがプレイヤーを追従する処理を追加しました。

--- GhostGame/Assets/Scripts/Player/State/Move.cs
+++ GhostGame/Assets/Scripts/Player/State/Move.cs
@@ -2,60 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : IState
+namespace personal
 {
-    private PlayerController player;
-    public Vector3 velocity;
 
-    public Move(PlayerController player)
+    public class Move : IState
     {
-        this.player = player;
+        private PlayerController player;
+        private Camera camera;
+        public Vector3 velocity;
+
+        public Move(PlayerController player)
+        {
+            this.player = player;
+        }
+
+        public void Enter()
+        {
+            camera= Camera.main;
+        }
+
+        public void Update()
+        {
+            bool ISmove = false;
+            velocity = new Vector3(0, 0, 0);
+            // カメラから見た左右と前後の入力値を受け取る
+            velocity += Input.GetAxis("Horizontal") * new Vector3(camera.transform.right.x, 0.0f, camera.transform.right.z).normalized;
+            velocity += Input.GetAxis("Vertical") * new Vector3(camera.transform.forward.x, 0.0f, camera.transform.forward.z).normalized;
+            velocity = velocity.normalized * player.Speed;
+            if(velocity!=Vector3.zero)
+            {
+                // プレイヤーの向きを移動方向へ向かせる
+                player.transform.forward = velocity.normalized;
+                // 移動フラグをON
+                ISmove = true;
+            }
+            
+            player.Rb.velocity = velocity;
+
+            if (Input.GetKeyDown("joystick button 2"))
+            {
+                player.Change(player.lightAttack);
+            }
+            if (Input.GetKeyDown("joystick button 4"))
+            {
+                player.Change(player.stanAttack);
+            }
+            // このフレームで移動入力がなかったなら移動ステートから抜ける
+            if (ISmove == false)
+            {
+                player.Change(player.idle);
+            }
+        }
+
+        public void Exit()
+        {
+            velocity = new Vector3(0, 0, 0);
+           player.Rb.velocity = new Vector3(0, 0, 0);
+        }
+
     }
-
-    public void Enter()
-    {
-
-    }
-
-    public void Update()
-    {
-        bool ISmove = false;
-        velocity = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity += player.transform.forward * player.Speed;
-            ISmove = true;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            velocity += -player.transform.forward * player.Speed;
-            ISmove = true;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            velocity += player.transform.right * player.Speed;
-            ISmove = true;
-
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            velocity += -player.transform.right * player.Speed;
-            ISmove = true;
-
-        }
-
-        player.Rb.velocity = velocity;
-        if(ISmove == false)
-        {
-            player.Change(player.idle);
-        }
-    }
-
-    public void Exit()
-    {
-        velocity = new Vector3(0, 0, 0);
-       player.Rb.velocity = new Vector3(0, 0, 0);
-    }
-
 }
