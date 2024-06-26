35be7386c32fa3d74ed5a857f5c69418ed3ab951
hikarukajita <8pika.k@gmail.com>
2024-06-26 10:32:34 +0900

    UIの追加と変更です

---
+++ GhostGame/Assets/Scripts/Enemy/EnemyUIOnOff.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUIOnOff : MonoBehaviour
+{
+    public GameObject obj;
+    private EnemyBase enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy.hp < enemy.maxHp)
+        {
+            obj.SetActive(true);
+
+        }
+       
+
+        if(enemy.stanPoint < enemy.maxStanPoint)
+        {
+            obj.SetActive(true);
+        }
+       
+    }
+
+    public void SetUIEnemy(EnemyBase enemy)
+    {
+        this.enemy = enemy;
+    }
+
+}
