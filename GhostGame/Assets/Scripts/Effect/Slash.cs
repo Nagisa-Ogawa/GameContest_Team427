6843b7d99244aed57d9be748c15c8d29e61c5dd4
Ogawa-Nagisa <o.nagisa0123@gmail.com>
2024-07-03 09:38:49 +0900

    ・プレイヤーの弱攻撃が3段階までコンボできる処理を追加しました。

---
+++ GhostGame/Assets/Scripts/Effect/Slash.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slash : MonoBehaviour
+{
+    // デバック用　コンボごとのエフェクトの角度
+    public Vector3[] comboSlashRot = new Vector3[3];
+
+}
